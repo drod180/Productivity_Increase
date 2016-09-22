21d9c670efa2e7026af8ceebba118dd420194b71
drod180 <719drod@gmail.com>
2016-09-22 13:18:57 -0400

    Add specific URL filtering

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(info) {
+var urls = ["https://www.facebook.com/", "http://na.op.gg/"];
 
+urls.forEach(function(url) {
+  chrome.webNavigation.onCommitted.addListener(function(info) {
+    chrome.tabs.update(info.tabId, {url: "about:blank"});
+  },
+  {
+    url: [{
+      urlPrefix: url
+    }],
   });
-},
-{
-  urls: [
-    "<all_urls>"
-  ],
-});
+})
