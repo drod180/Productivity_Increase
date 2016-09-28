cce709e7fd62e062eab857c3874c71e54c1d91c0
drod180 <719drod@gmail.com>
2016-09-28 16:52:23 -0400

    Add more navigation events

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
 var urls = ["https://www.facebook.com/", "http://na.op.gg/"];
 
+var eventList = ['onBeforeNavigate', 'onCreatedNavigationTarget',
+    'onCommitted', 'onCompleted'];
+
+var funTime = 3;
+
 urls.forEach(function(url) {
-  chrome.webNavigation.onCommitted.addListener(function(info) {
-    chrome.tabs.update(info.tabId, {url: "about:blank"});
-  },
-  {
-    url: [{
-      urlPrefix: url
-    }],
+  eventList.forEach(function(e) {
+    chrome.webNavigation[e].addListener(function(info) {
+      if (funTime === 0) {
+        //chrome.tabs.update(info.tabId, {url: "about:blank"});
+        alert(e + " " + info.url);
+      }
+    },
+    {
+      url: [{
+        urlPrefix: url
+      }],
+    });
   });
-})
+});
