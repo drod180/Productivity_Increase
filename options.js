eeea7a3ba82baf9d8402d7e84a8d90dfdba05b2b
drod180 <719drod@gmail.com>
2016-10-05 20:36:41 -0400

    Add loading and saving options data

--- options.js
+++ options.js
@@ -5,13 +5,20 @@
 * Maximum break time
 */
 function saveOptions() {
+  var options = _getOptions();
   chrome.storage.sync.set({
     options: options
-  })
+  }, function() {
+    console.log("Data saved!");
+  });
 }
 
 function loadOptions() {
-
+  chrome.storage.sync.get("options", function (obj) {
+    $("#break-ratio-dropdown option[value="+ obj.options.ratio + "]").prop("selected", true);
+    $("#max-break-dropdown option[value="+ obj.options.maxBr + "]").prop("selected", true);;
+    $("#min-break-dropdown option[value="+ obj.options.minBr + "]").prop("selected", true);;
+  });
 }
 
 function addSite() {
@@ -21,3 +28,18 @@ function addSite() {
 function removeSite() {
 
 }
+
+function _getOptions() {
+  var options = {};
+
+//  options.sites = $('.sites-list');
+  options.ratio = $('#break-ratio-dropdown').val();
+  options.maxBr = $('#max-break-dropdown').val();
+  options.minBr = $('#min-break-dropdown').val();
+
+  return options;
+}
+
+$(document).ready(function () {
+  loadOptions();
+});
